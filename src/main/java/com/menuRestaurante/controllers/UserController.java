0ef4f8c2e40052fbdfb602e2a126f20afd6b7b5f
Yainsydeveloper <yainsycontreras@gmail.com>
2023-01-03 18:32:31 -0300

    Agregue Thymeleaf la conexion al inicio

---
+++ src/main/java/com/menuRestaurante/controllers/UserController.java
@@ -0,0 +1,97 @@
+package com.menuRestaurante.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.menuRestaurante.models.User;
+import com.menuRestaurante.services.UserService;
+
+@RestController
+public class UserController {
+
+private final UserService userService;
+
+public UserController(UserService userService) {
+   this.userService = userService;
+}
+
+@GetMapping("/api/user/{id}")
+@ResponseStatus(code = HttpStatus.OK)
+public Optional<User> byId(@PathVariable("id") int id) {
+   return userService.find(id);
+}
+
+@GetMapping("/api/user")
+@ResponseStatus(code = HttpStatus.OK)
+public List<User> getUsers(){
+return (List<User>) userService.findAll();
+}
+
+@PostMapping("/api/user")
+@ResponseStatus(code = HttpStatus.CREATED)
+public User create(@RequestBody User user) {
+   return userService.create(user);
+}
+
+@PutMapping("/api/user/")
+@ResponseStatus(code = HttpStatus.ACCEPTED)
+public User update(@RequestBody User user) {
+   return userService.update(user);
+}
+
+@PatchMapping("/api/user/")
+@ResponseStatus(code = HttpStatus.ACCEPTED)
+public User change(@RequestBody User user) {
+   return userService.change(user);
+}
+
+@DeleteMapping("/api/user/{id}")
+@ResponseStatus(code = HttpStatus.OK)
+public Boolean delete(@PathVariable("id") int id) {
+   return userService.remove(id);
+}
+// Con Swagger
+
+/*
+@GetMapping("/api/user")
+public ResponseEntity<List<User>> findAll(){
+return ResponseEntity.ok((List<User>) userService.findAll());
+}
+
+@GetMapping("/api/user/{id}")
+public ResponseEntity<Optional<User>> findById(@PathVariable("id") int id){
+return ResponseEntity.ok(userService.find(id));
+}
+
+
+@PutMapping("/api/user/")
+public ResponseEntity<User> update(User user){
+userService.update(user);
+return ResponseEntity.ok(user);
+}
+
+@PostMapping("/api/user")
+public ResponseEntity<User> create(User user){
+userService.create(user);
+return ResponseEntity.ok(user);
+}
+
+@DeleteMapping("/api/user/{id}")
+public ResponseEntity<?> delete( @PathVariable("id") int id ) {
+userService.remove(id);
+return ResponseEntity.ok().build();
+
+}*/
+
+}
---
+++ src/main/java/com/menuRestaurante/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.menuRestaurante.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.menuRestaurante.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
